5625bc703f3349bb2c9f880122910701f0fc92aa
rahul7045 <rahulpujari525@gmail.com>
2023-01-20 20:53:01 +0530

    exporting modules

--- 06-Node js/01-node-start/03-node-basics/app.js
+++ 06-Node js/01-node-start/03-node-basics/app.js
@@ -1,12 +1,11 @@
 const http = require('http')
-const fs = require('fs')
+const routes = require('./routes')
 
 
 const server = http.createServer((req , res)=>{
   //console.log(req.url , req.headers , req.method)
   res.setHeader('Content-Type' , 'text/html')
-  const url = req.url
-  const method = req.method
+  
   // if(url==='/home'){
   //   res.write('<html>')
   //   res.write('<head><title>My Home page</title></head>')
@@ -27,39 +26,8 @@ const server = http.createServer((req , res)=>{
   //   return res.end()
   // }
 
-
-  if(url==='/'){
-    const data = fs.readFileSync('./message.txt', 'utf8');
-    res.write('<html>')
-    res.write('<head><title>Enter Message</title></head>')
-    res.write(`<body><form action="/message" method="POST"><div>${data}</div><input type="text" name="message" /><button type="submit" >Submit</button></form></body>`)
-    res.write('</html>')
-    return res.end()
-  }
-
-  if(url==='/message' && method==='POST'){
-    const body=[];
-    req.on('data',(chunk)=>{
-      console.log(chunk);
-      body.push(chunk)
-    })
-    req.on('end',()=>{
-      const parseBody = Buffer.concat(body).toString();
-      const message = parseBody.split("=")[1];
-      console.log(message)
-      fs.writeFileSync('message.txt' , message)
-    })
-    fs.writeFileSync('hello.txt', "DUMMY")
-    res.statusCode= 302;
-    res.setHeader('Location', '/')
-    return res.end()
-  }
-
-  res.write('<html>')
-  res.write('<head><title>My first page</title></head>')
-  res.write('<body>Welcome to my Node js Project</body>')
-  res.write('</html>')
-  res.end()
+routes.handler(req , res)
+ 
 
 })
 server.listen(4000)
