0c657b8f1fd79394b62e724657b1cff75efbd0dc
rahul7045 <rahulpujari525@gmail.com>
2023-01-20 17:12:46 +0530

    added routes in node

--- 06-Node js/01-node-start/03-node-basics/app.js
+++ 06-Node js/01-node-start/03-node-basics/app.js
@@ -3,6 +3,33 @@ const http = require('http')
 
 
 const server = http.createServer((req , res)=>{
-  console.log("Rahul")
+  //console.log(req.url , req.headers , req.method)
+  res.setHeader('Content-Type' , 'text/html')
+  const url = req.url
+  if(url==='/home'){
+    res.write('<html>')
+    res.write('<head><title>My Home page</title></head>')
+    res.write('<body>Welcome to  Home Page</body>')
+    res.write('</html>')
+   return  res.end()
+  }else if(url==='/about'){
+    res.write('<html>')
+    res.write('<head><title>My About page</title></head>')
+    res.write('<body>Welcome to About Page</body>')
+    res.write('</html>')
+   return  res.end()
+  }else if(url==='/node'){
+    res.write('<html>')
+    res.write('<head><title>My Node page</title></head>')
+    res.write('<body>Welcome to my Node js Project</body>')
+    res.write('</html>')
+    return res.end()
+  }
+  res.write('<html>')
+  res.write('<head><title>My first page</title></head>')
+  res.write('<body>Welcome to my Node js Project</body>')
+  res.write('</html>')
+  res.end()
+
 })
 server.listen(4000)
