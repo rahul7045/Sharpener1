4fdaeb70584e51b05f0ec7a4237f430789d10293
rahul7045 <rahulpujari525@gmail.com>
2023-01-26 01:02:21 +0530

    added controller

--- 06-Node js/02-express-js/01-express-start/routes/contact.js
+++ 06-Node js/02-express-js/01-express-start/routes/contact.js
@@ -1,21 +1,13 @@
 const express = require('express')
 const path = require('path')
 const rootDir = require('../utils/path')
+const contactController = require('../controllers/contact')
 
 const router = express.Router();
 
-router.use('/contactus',(req , res , next)=>{
-    res.sendFile(path.join(rootDir  , 'views' , 'contact.html'))
-
-  //  res.send('<form action="/admin/products" method="POST"><input type="text" name="title" /><input type="number" name="quantity" /><button type="submit">Add </button></form>')
-  })
+router.use('/contactus',contactController.formPage)
   
-  router.post('/contact-form',(req , res , next)=>{
-      console.log(req.body)
-     // res.redirect('/')
-     res.send('<h1>Success</h1>')
-     // next();
-  })
+  router.post('/contact-form',contactController.formControl)
 
 
 module.exports = router
