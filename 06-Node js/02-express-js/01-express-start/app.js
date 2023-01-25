072cda858b051c1af829ecbec8420f77dbbb4da7
rahul7045 <rahulpujari525@gmail.com>
2023-01-26 00:15:19 +0530

    added contact and html files

--- 06-Node js/02-express-js/01-express-start/app.js
+++ 06-Node js/02-express-js/01-express-start/app.js
@@ -1,21 +1,29 @@
 const http = require('http')
+const path = require('path')
 
 const express = require('express')
 const bodyParser = require('body-parser')
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
+const contactRoutes = require('./routes/contact')
+const rootDir = require('./utils/path')
+
 
 
 const app =express()
 
 app.use(bodyParser.urlencoded({extended : false}))
 
+app.use(express.static(path.join(__dirname , 'public')))
+
 app.use('/admin',adminRoutes)
 
 app.use(shopRoutes)
+app.use(contactRoutes)
 
 app.use((req , res , next)=>{
-  res.send('<h1>Page Not Found</h1>')
+  res.sendFile(path.join(rootDir ,'views', 'pageNotFound.html'))
+ // res.send('<h1>Page Not Found</h1>')
 })
 
 
